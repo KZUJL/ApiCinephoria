cf8fd66e98f9917ca0573db2ef0188d23efe069d
Jean-Loup <jean-loup.quazuguel@zf.com>
2025-09-09 09:36:11 +0200

    suppression de l'importe des bases

--- ApiCinephoria/Program.cs
+++ ApiCinephoria/Program.cs
@@ -81,18 +81,18 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // --- Seeder forcé : supprime toutes les tables et réimporte ---
-try
-{
-    var seeder = new DatabaseSeeder(mysqlConnectionString);
-    var sqlFolder = Path.Combine(AppContext.BaseDirectory, "Data"); // dossier contenant tous les fichiers SQL
-    Console.WriteLine("Début de l'import SQL (forcer la recréation des tables)...");
-    seeder.ImportSqlDumpForce(sqlFolder);
-    Console.WriteLine("Import SQL terminé !");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Erreur lors de l'import SQL : {ex.Message}");
-}
+//try
+//{
+//    var seeder = new DatabaseSeeder(mysqlConnectionString);
+//    var sqlFolder = Path.Combine(AppContext.BaseDirectory, "Data"); // dossier contenant tous les fichiers SQL
+//    Console.WriteLine("Début de l'import SQL (forcer la recréation des tables)...");
+//    seeder.ImportSqlDumpForce(sqlFolder);
+//    Console.WriteLine("Import SQL terminé !");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"Erreur lors de l'import SQL : {ex.Message}");
+//}
 
 if (app.Environment.IsDevelopment())
 {
